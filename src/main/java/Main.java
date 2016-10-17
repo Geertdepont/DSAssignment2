b7e1ad674effe479ef07d34de782363504e6d8bb
gatisnolv <gatisnolv@gmail.com>
2016-10-17 13:25:24 +0200

    Start commenting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,20 +2,37 @@ import java.io.FileNotFoundException;
 
 public class Main implements DS2Interface {
 	//
+	
+	/* General comments that apply to all methods:
+ 	 * 	1) Variable initialization takes constant time.
+	 * 	2) We tried to keep the algorithms as close to the provided (book and lectures) pseudo-code as possible
+	 *	3) The pseudo-code of the algorithms sometimes assumes that the first element of an array has index '1', 
+	 *	whereas in Java arrays start with the index '0', so where relevant we initialized counters in loops that 
+	 *	are 1 less than those used in the pseudo-code
+	 */
 
+	/* 	General comments about our recursiveRodCutting implementation:
+	 * 	Our implementation follows the pseudo-code very closely except for the difference in input, where as 
+	 * 	opposed to the pseudo-code, we only get the price list as input, and treat its length as the rod length, 
+	 * 	as per the assignment. Because of this, our implementation has the same time complexity as the pseudo-code,
+	 *  O(2^n)
+	 */
     @Override
     public int recursiveRodCutting(int[] input) {
         int length=input.length-1;
     	if(length==0){
         	return 0;
         }
-        int q=Integer.MIN_VALUE;
+        int q=Integer.MIN_VALUE;//Integer.MIN_VALUE stands for "negative infinity"
         for(int i=1;i<=length;i++){
         	q=max(q, (input[i]+recursiveRodCutting(copyArray(input, input.length-i))));
         }
     	return q;
     }
 
+    /*	This is a that method returns the largest of a variable number of integers that can be passed to it. 
+     *	It loops over the supplied integers checking each once, so it's time complexity is in Theta(n)
+     */
 	public int max(int... integers){
     	int result=Integer.MIN_VALUE;
     	for(int e: integers){
@@ -25,7 +42,11 @@ public class Main implements DS2Interface {
     	}
     	return result;
     }
-    
+	
+	 /*	This is a method that returns a copy of a subarray from the supplied array, starting from the first 
+	  * element up to specified by the length parameter.
+     *	It loops over the elements specified by the length parameter, so it's time complexity is Theta(length)
+     */
     public int[] copyArray(int[] input, int length){
     	int[] result=new int[length];
     	for(int i=0;i<length;i++){
@@ -34,6 +55,11 @@ public class Main implements DS2Interface {
     	return result;
     }
     
+	/* 	General comments about our dynamicRodCutting implementation:
+	 * 	Similarly to our implementation of recursiveRodCutting, our implementation of dynamicRodCutting closely 
+	 * 	follows the pseudo-code  except for the difference in input. Again, because of this, our implementation 
+	 * 	has the same time complexity as the pseudo-code, O(n^2)
+	 */
     @Override
     public int dynamicRodCutting(int[] input) {
     	int length=input.length-1;
